12e9caa8b6823d3efd499dd298edc91cbde00727
forrestwilkins <ethanforrestwilkins@gmail.com>
2021-11-14 15:41:04 -0500

    Fix

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -2,5 +2,5 @@ import { Environments } from "../constants/common";
 
 export const baseUrl =
   process.env.NODE_ENV === Environments.Production
-    ? process.env.PRODUCTION_BASE_URL
+    ? (process.env.PRODUCTION_BASE_URL as string)
     : "http://localhost:3000";
