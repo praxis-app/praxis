b964867c63c6cf88261e10efdee9151e8d32d27f
forrestwilkins <ethanforrestwilkins@gmail.com>
2021-12-05 19:06:15 -0500

    Added back baseUrl changes, with require dotenv

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,6 +1,8 @@
 import { Environments } from "../constants/common";
 
+require("dotenv").config();
+
 export const baseUrl =
   process.env.NODE_ENV === Environments.Production
-    ? "https://p.raxis.xyz"
+    ? (process.env.PRODUCTION_BASE_URL as string)
     : "http://localhost:3000";
