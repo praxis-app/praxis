1dd34cb45799013831841157af31201cf7a67e6e
forrestwilkins <ethanforrestwilkins@gmail.com>
2022-01-09 10:44:22 -0500

    Attempt to fix issue with publicRuntimeConfig at build

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,11 +1,9 @@
 import { Environments } from "../constants/common";
 import getConfig from "next/config";
 
-const {
-  publicRuntimeConfig: { PRODUCTION_BASE_URL },
-} = getConfig();
+const { publicRuntimeConfig } = getConfig();
 
 export const baseUrl =
-  process.env.NODE_ENV === Environments.Production
-    ? (PRODUCTION_BASE_URL as string)
+  process.env.NODE_ENV === Environments.Production && publicRuntimeConfig
+    ? (publicRuntimeConfig.PRODUCTION_BASE_URL as string)
     : "http://localhost:3000";
