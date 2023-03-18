3573257d65d1ea0d4b15a67d129f189a5d5c36b1
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-03-17 20:55:25 -0400

    refactor: add todo comment for adding create-like.input fields

--- src/likes/models/create-like.input.ts
+++ src/likes/models/create-like.input.ts
@@ -2,6 +2,8 @@ import { Field, InputType, Int } from "@nestjs/graphql";
 
 @InputType()
 export class CreateLikeInput {
+  // TODO: Add ID fields for comments and other models when added
+
   @Field(() => Int, { nullable: true })
   postId: number;
 }
