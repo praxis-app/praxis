6b479afc0e1955b2602cf33f4185f6a9c624714d
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-03-17 20:57:22 -0400

    refactor: add todo comment for adding delete-like.input fields

--- src/likes/models/create-like.input.ts
+++ src/likes/models/create-like.input.ts
@@ -1,9 +1,9 @@
+// TODO: Add ID fields for comments and other models when added
+
 import { Field, InputType, Int } from "@nestjs/graphql";
 
 @InputType()
 export class CreateLikeInput {
-  // TODO: Add ID fields for comments and other models when added
-
   @Field(() => Int, { nullable: true })
   postId: number;
 }
--- src/likes/models/delete-like.input.ts
+++ src/likes/models/delete-like.input.ts
@@ -1,3 +1,5 @@
+// TODO: Add ID fields for comments and other models when added
+
 import { Field, InputType, Int } from "@nestjs/graphql";
 
 @InputType()
