c133a987582a09590efd7579b5c6d580d6271e99
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-04-10 20:34:09 -0400

    chore: add migrations for converting role member relation to many to many

---
+++ src/database/migrations/1681172948650-AddRoleMemberLinkTable.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddRoleMemberLinkTable1681172948650 implements MigrationInterface {
+  name = "AddRoleMemberLinkTable1681172948650";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "role_members_user" ("roleId" integer NOT NULL, "userId" integer NOT NULL, CONSTRAINT "PK_b47ecc28f78e95361c666b11fa8" PRIMARY KEY ("roleId", "userId"))`
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_bc4c45c917cd69cef0574dc3c0" ON "role_members_user" ("roleId") `
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_8ebd83d04eb1d0270c6e1d9d62" ON "role_members_user" ("userId") `
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_members_user" ADD CONSTRAINT "FK_bc4c45c917cd69cef0574dc3c0a" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE CASCADE ON UPDATE CASCADE`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_members_user" ADD CONSTRAINT "FK_8ebd83d04eb1d0270c6e1d9d620" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "role_members_user" DROP CONSTRAINT "FK_8ebd83d04eb1d0270c6e1d9d620"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_members_user" DROP CONSTRAINT "FK_bc4c45c917cd69cef0574dc3c0a"`
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_8ebd83d04eb1d0270c6e1d9d62"`
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_bc4c45c917cd69cef0574dc3c0"`
+    );
+    await queryRunner.query(`DROP TABLE "role_members_user"`);
+  }
+}
---
+++ src/database/migrations/1681173025669-DropRoleMemberEntityTable.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class DropRoleMemberEntityTable1681173025669
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "role_member" DROP CONSTRAINT "FK_d8d704a0c5445a41ff5ec5a6f26"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_member" DROP CONSTRAINT "FK_e615f68e242fd1add54be0032d2"`
+    );
+    await queryRunner.query(`DROP TABLE "role_member"`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "role_member" ("id" SERIAL NOT NULL, "roleId" integer NOT NULL, "userId" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_768f9d166547a51dbb0e3fee645" PRIMARY KEY ("id"))`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_member" ADD CONSTRAINT "FK_e615f68e242fd1add54be0032d2" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE CASCADE ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_member" ADD CONSTRAINT "FK_d8d704a0c5445a41ff5ec5a6f26" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE NO ACTION`
+    );
+  }
+}
