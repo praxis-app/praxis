a4cfcb9dad02c4e509fd95c937a5599cf5b3f00c
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-06-27 19:47:50 -0400

    refactor: remove unused enums

--- src/server-roles/server-roles.constants.ts
+++ src/server-roles/server-roles.constants.ts
@@ -1,25 +1,2 @@
 export const DEFAULT_ROLE_COLOR = "#f44336";
 export const ADMIN_ROLE_NAME = "admin";
-
-export enum ServerPermission {
-  BanMembers = "ban-members",
-  CreateInvites = "create-invites",
-  ManageComments = "manage-comments",
-  ManageEvents = "manage-events",
-  ManageInvites = "manage-invites",
-  ManagePosts = "manage-posts",
-  ManageRoles = "manage-roles",
-}
-
-export enum GroupPermission {
-  ApproveMemberRequests = "approve-group-member-requests",
-  BanMembers = "ban-group-members",
-  CreateEvents = "create-group-events",
-  DeleteGroup = "delete-group",
-  UpdateGroup = "update-group",
-  ManageComments = "manage-group-comments",
-  ManageEvents = "manage-group-events",
-  ManagePosts = "manage-group-posts",
-  ManageRoles = "manage-group-roles",
-  ManageSettings = "manage-group-settings",
-}
