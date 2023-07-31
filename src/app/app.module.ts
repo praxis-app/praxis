78e851f888384ddf95f687613cae415fd5efaed7
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-07-30 21:09:24 -0400

    refactor: add app service with getContext method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { ApolloModule } from "./apollo.module";
 import { AuthModule } from "../auth/auth.module";
+import { RefreshTokensModule } from "../auth/refresh-tokens/refresh-tokens.module";
+import { ShieldModule } from "../auth/shield/shield.module";
 import { DatabaseModule } from "../database/database.module";
 import { DataloaderModule } from "../dataloader/dataloader.module";
 import { EventsModule } from "../events/events.module";
+import { GroupMemberRequestsModule } from "../groups/group-member-requests/group-member-requests.module";
+import { GroupRolesModule } from "../groups/group-roles/group-roles.module";
 import { GroupsModule } from "../groups/groups.module";
 import { ImagesModule } from "../images/images.module";
 import { LikesModule } from "../likes/likes.module";
@@ -14,6 +17,7 @@ import { ServerInvitesModule } from "../server-invites/server-invites.module";
 import { ServerRolesModule } from "../server-roles/server-roles.module";
 import { UsersModule } from "../users/users.module";
 import { VotesModule } from "../votes/votes.module";
+import { ApolloModule } from "./apollo.module";
 
 @Module({
   imports: [
@@ -23,13 +27,17 @@ import { VotesModule } from "../votes/votes.module";
     DatabaseModule,
     DataloaderModule,
     EventsModule,
+    GroupMemberRequestsModule,
+    GroupRolesModule,
     GroupsModule,
     ImagesModule,
     LikesModule,
     PostsModule,
     ProposalsModule,
+    RefreshTokensModule,
     ServerInvitesModule,
     ServerRolesModule,
+    ShieldModule,
     UsersModule,
     VotesModule,
   ],
---
+++ src/app/app.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from "@nestjs/common";
+import { getClaims, getSub } from "../auth/auth.utils";
+import { RefreshTokensService } from "../auth/refresh-tokens/refresh-tokens.service";
+import { ShieldService } from "../auth/shield/shield.service";
+import { Context, ContextServices } from "../common/common.types";
+import { DataloaderService } from "../dataloader/dataloader.service";
+import { EventsService } from "../events/events.service";
+import { GroupMemberRequestsService } from "../groups/group-member-requests/group-member-requests.service";
+import { GroupRolesService } from "../groups/group-roles/group-roles.service";
+import { GroupsService } from "../groups/groups.service";
+import { PostsService } from "../posts/posts.service";
+import { ProposalsService } from "../proposals/proposals.service";
+import { UsersService } from "../users/users.service";
+
+@Injectable()
+export class AppService {
+  constructor(
+    private dataloaderService: DataloaderService,
+    private eventsService: EventsService,
+    private groupMemberRequestsService: GroupMemberRequestsService,
+    private groupRolesService: GroupRolesService,
+    private groupsService: GroupsService,
+    private postsService: PostsService,
+    private proposalsService: ProposalsService,
+    private refreshTokensService: RefreshTokensService,
+    private shieldService: ShieldService,
+    private usersService: UsersService
+  ) {}
+
+  async getContext({ req }: { req: Request }): Promise<Context> {
+    const claims = getClaims(req);
+    const sub = getSub(claims.accessTokenClaims);
+    const permissions = sub
+      ? await this.usersService.getUserPermissions(sub)
+      : null;
+    const user = sub ? await this.usersService.getUser({ id: sub }) : null;
+
+    const services: ContextServices = {
+      eventsService: this.eventsService,
+      groupMemberRequestsService: this.groupMemberRequestsService,
+      groupRolesService: this.groupRolesService,
+      groupsService: this.groupsService,
+      postsService: this.postsService,
+      proposalsService: this.proposalsService,
+      refreshTokensService: this.refreshTokensService,
+      shieldService: this.shieldService,
+      usersService: this.usersService,
+    };
+    const loaders = this.dataloaderService.getLoaders();
+
+    return {
+      claims,
+      loaders,
+      permissions,
+      services,
+      user,
+    };
+  }
+}
