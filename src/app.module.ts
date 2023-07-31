cfd9881b8bcbd0957514cb24ce4392cb4e7f10c5
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-07-30 22:46:13 -0400

    refactor: remove uneeded imports from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,6 @@ import { GraphQLSchema } from "graphql";
 import { applyMiddleware } from "graphql-middleware";
 import { GraphQLUpload } from "graphql-upload";
 import { AuthModule } from "./auth/auth.module";
-import { RefreshTokensModule } from "./auth/refresh-tokens/refresh-tokens.module";
-import { ShieldModule } from "./auth/shield/shield.module";
 import { shieldPermissions } from "./auth/shield/shield.permissions";
 import { Environment } from "./common/common.constants";
 import { ContextModule } from "./context/context.module";
@@ -15,8 +13,6 @@ import { ContextService } from "./context/context.service";
 import { DatabaseModule } from "./database/database.module";
 import { DataloaderModule } from "./dataloader/dataloader.module";
 import { EventsModule } from "./events/events.module";
-import { GroupMemberRequestsModule } from "./groups/group-member-requests/group-member-requests.module";
-import { GroupRolesModule } from "./groups/group-roles/group-roles.module";
 import { GroupsModule } from "./groups/groups.module";
 import { ImagesModule } from "./images/images.module";
 import { LikesModule } from "./likes/likes.module";
@@ -54,17 +50,13 @@ export const ApolloModule = GraphQLModule.forRootAsync<ApolloDriverConfig>({
     DatabaseModule,
     DataloaderModule,
     EventsModule,
-    GroupMemberRequestsModule,
-    GroupRolesModule,
     GroupsModule,
     ImagesModule,
     LikesModule,
     PostsModule,
     ProposalsModule,
-    RefreshTokensModule,
     ServerInvitesModule,
     ServerRolesModule,
-    ShieldModule,
     UsersModule,
     VotesModule,
   ],
