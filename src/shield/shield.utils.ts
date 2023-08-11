d7d2d54bfe457ffef6bd366a44d038b004e08c3b
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-08-10 21:27:17 -0400

    refactor: remove unused shield utils

--- src/shield/shield.utils.ts
+++ src/shield/shield.utils.ts
@@ -37,14 +37,3 @@ export const hasNodes = (nodes: string[], path: ResponsePath) => {
   const pathArray = responsePathAsArray(path);
   return nodes.every((node) => pathArray.includes(node));
 };
-
-// TODO: Determine whether to remove this function
-export const getPath = (path: ResponsePath) =>
-  responsePathAsArray(path).reduce<string>(
-    (result, segment) => `${result}${result ? "." : ""}${segment}`,
-    ""
-  );
-
-// TODO: Determine whether to remove this function
-export const hasPath = (path: RegExp, currentPath: ResponsePath) =>
-  path.test(getPath(currentPath));
