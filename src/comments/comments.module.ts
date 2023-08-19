4e973dce1b8f6fcf4ccb4c550ac8237526e0e534
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-08-19 18:23:44 -0400

    feat: add create, update, delete mutations for comments

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
-import { CommentsService } from "./comments.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ImagesModule } from "../images/images.module";
 import { CommentsResolver } from "./comments.resolver";
+import { CommentsService } from "./comments.service";
+import { Comment } from "./models/comment.model";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Comment]), ImagesModule],
   providers: [CommentsService, CommentsResolver],
 })
 export class CommentsModule {}
--- src/comments/comments.resolver.ts
+++ src/comments/comments.resolver.ts
@@ -1,4 +1,31 @@
-import { Resolver } from "@nestjs/graphql";
+import { Args, Int, Mutation, Resolver } from "@nestjs/graphql";
+import { CurrentUser } from "../auth/decorators/current-user.decorator";
+import { User } from "../users/models/user.model";
+import { CommentsService } from "./comments.service";
+import { CreateCommentInput } from "./models/create-comment.input";
+import { CreateCommentPayload } from "./models/create-comment.payload";
+import { UpdateCommentInput } from "./models/update-comment.input";
+import { UpdateCommentPayload } from "./models/update-comment.payload";
 
 @Resolver()
-export class CommentsResolver {}
+export class CommentsResolver {
+  constructor(private commentsService: CommentsService) {}
+
+  @Mutation(() => CreateCommentPayload)
+  async createComment(
+    @Args("commentData") commentData: CreateCommentInput,
+    @CurrentUser() user: User
+  ) {
+    return this.commentsService.createComment(commentData, user);
+  }
+
+  @Mutation(() => UpdateCommentPayload)
+  async updateComment(@Args("commentData") commentData: UpdateCommentInput) {
+    return this.commentsService.updateComment(commentData);
+  }
+
+  @Mutation(() => Boolean)
+  async deleteComment(@Args("id", { type: () => Int }) id: number) {
+    return this.commentsService.deleteComment(id);
+  }
+}
