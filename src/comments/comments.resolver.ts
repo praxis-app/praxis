02475515ecbb003797a7da85a9face33fd97e2ba
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-08-19 18:25:36 -0400

    feat: add user field resolver for comments

--- src/comments/comments.resolver.ts
+++ src/comments/comments.resolver.ts
@@ -1,7 +1,17 @@
-import { Args, Int, Mutation, Resolver } from "@nestjs/graphql";
+import {
+  Args,
+  Context,
+  Int,
+  Mutation,
+  Parent,
+  ResolveField,
+  Resolver,
+} from "@nestjs/graphql";
 import { CurrentUser } from "../auth/decorators/current-user.decorator";
+import { Dataloaders } from "../dataloader/dataloader.types";
 import { User } from "../users/models/user.model";
 import { CommentsService } from "./comments.service";
+import { Comment } from "./models/comment.model";
 import { CreateCommentInput } from "./models/create-comment.input";
 import { CreateCommentPayload } from "./models/create-comment.payload";
 import { UpdateCommentInput } from "./models/update-comment.input";
@@ -10,6 +20,14 @@ import { UpdateCommentPayload } from "./models/update-comment.payload";
 @Resolver()
 export class CommentsResolver {
   constructor(private commentsService: CommentsService) {}
+
+  @ResolveField(() => User)
+  async user(
+    @Context() { loaders }: { loaders: Dataloaders },
+    @Parent() { userId }: Comment
+  ) {
+    return loaders.usersLoader.load(userId);
+  }
 
   @Mutation(() => CreateCommentPayload)
   async createComment(
