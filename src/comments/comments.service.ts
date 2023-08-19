8492025116e1c0b352d3d0ebf652c67aae841893
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-08-19 18:00:54 -0400

    feat: add getComment and getComments methods

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { FindOptionsWhere, Repository } from "typeorm";
+import { Comment } from "./models/comment.model";
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    @InjectRepository(Comment)
+    private repository: Repository<Comment>
+  ) {}
+
+  async getComment(id: number, relations?: string[]) {
+    return this.repository.findOneOrFail({ where: { id }, relations });
+  }
+
+  async getComments(where?: FindOptionsWhere<Comment>) {
+    return this.repository.find({ where, order: { createdAt: "DESC" } });
+  }
+}
