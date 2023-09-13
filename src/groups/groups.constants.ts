5592fb270a6773f68fb59ac473b7fa045a8cf406
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-09-12 21:50:48 -0400

    refactor: revert changes to constants from testing

--- src/groups/groups.constants.ts
+++ src/groups/groups.constants.ts
@@ -1,8 +1,5 @@
-// TODO: Add support for group settings
-
 export enum DefaultGroupSetting {
-  // TODO: Switch back to 75 after testing
-  RatificationThreshold = 25,
+  RatificationThreshold = 75,
   ReservationsLimit = 2,
   StandAsidesLimit = 2,
 }
