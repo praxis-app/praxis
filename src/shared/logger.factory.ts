2486aab288a1022ca822df17444a17e3b48474e9
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-10-15 16:24:04 -0400

    refactor: split transports into separate vars in logger factory

--- src/shared/logger.factory.ts
+++ src/shared/logger.factory.ts
@@ -1,39 +1,45 @@
 import { WinstonModule, utilities } from 'nest-winston';
 import { format, transports } from 'winston';
+import { Environment } from './shared.constants';
 
-export const LoggerFactory = () =>
-  WinstonModule.createLogger({
-    level: 'info',
-
-    transports: [
-      new transports.File({
-        filename: `${__dirname}/../../logs/app.log`,
-        format: format.combine(format.timestamp(), format.json()),
-      }),
-
-      new transports.File({
-        level: 'error',
-        filename: `${__dirname}/../../logs/error.log`,
-        format: format.combine(format.timestamp(), format.json()),
-      }),
-
-      new transports.Console({
-        format: format.combine(
-          format.timestamp(),
-
-          // TODO: Remove when ready
-          // format.cli(),
-          // format.splat(),
-          // format.align(),
-
-          format.printf((info) => {
-            return `${info.timestamp} ${info.level}: ${info.message}`;
-          }),
-          utilities.format.nestLike('Praxis', {
-            colors: true,
-            prettyPrint: true,
-          }),
-        ),
-      }),
-    ],
+export const LoggerFactory = () => {
+  const appTransport = new transports.File({
+    filename: `${__dirname}/../../logs/app.log`,
+    format: format.combine(format.timestamp(), format.json()),
   });
+
+  const errorTransport = new transports.File({
+    level: 'error',
+    filename: `${__dirname}/../../logs/error.log`,
+    format: format.combine(format.timestamp(), format.json()),
+  });
+
+  const consoleTransport = new transports.Console({
+    format: format.combine(
+      format.timestamp(),
+
+      // TODO: Remove when ready
+      // format.cli(),
+      // format.splat(),
+      // format.align(),
+
+      format.printf((info) => {
+        return `${info.timestamp} ${info.level}: ${info.message}`;
+      }),
+      utilities.format.nestLike('Praxis', {
+        colors: true,
+        prettyPrint: true,
+      }),
+    ),
+  });
+
+  const allTransports =
+    process.env.NODE_ENV === Environment.Production
+      ? [appTransport, errorTransport]
+      : [consoleTransport];
+
+  return WinstonModule.createLogger({
+    transports: allTransports,
+    level: 'info',
+  });
+};
