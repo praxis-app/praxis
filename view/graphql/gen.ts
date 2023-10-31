346b1693d1242b0d4365661cce29f537a93277ff
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-10-30 21:36:32 -0400

    feat: add manageSettings field for server permissions

--- view/graphql/gen.ts
+++ view/graphql/gen.ts
@@ -878,6 +878,7 @@ export type ServerPermissions = {
   manageInvites: Scalars['Boolean']['output'];
   managePosts: Scalars['Boolean']['output'];
   manageRoles: Scalars['Boolean']['output'];
+  manageSettings: Scalars['Boolean']['output'];
   removeMembers: Scalars['Boolean']['output'];
 };
 
@@ -901,6 +902,7 @@ export type ServerRolePermission = {
   manageInvites: Scalars['Boolean']['output'];
   managePosts: Scalars['Boolean']['output'];
   manageRoles: Scalars['Boolean']['output'];
+  manageSettings: Scalars['Boolean']['output'];
   removeMembers: Scalars['Boolean']['output'];
   serverRole: ServerRole;
 };
@@ -912,6 +914,7 @@ export type ServerRolePermissionInput = {
   manageInvites?: InputMaybe<Scalars['Boolean']['input']>;
   managePosts?: InputMaybe<Scalars['Boolean']['input']>;
   manageRoles?: InputMaybe<Scalars['Boolean']['input']>;
+  manageSettings?: InputMaybe<Scalars['Boolean']['input']>;
   removeMembers?: InputMaybe<Scalars['Boolean']['input']>;
 };
 
