89c235cb38656a3b4f8ed36d94c44610dffc3cc5
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-11-11 13:31:41 -0500

    refactor: sort const in image constants file

--- src/images/image.constants.ts
+++ src/images/image.constants.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_IMAGES_SIZE = 10;
 
+export const VALID_IMAGE_FORMAT = /(jpe?g|png|gif|webp)$/;
+
 export const MAX_IMAGE_SIZE = 5 * 1024 * 1024; // 5MB
 export const MAX_IMAGE_COUNT = 5;
-
-export const VALID_IMAGE_FORMAT = /(jpe?g|png|gif|webp)$/;
