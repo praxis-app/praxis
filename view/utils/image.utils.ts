2b1b27256a6f3e9e91ac401e672b59b427284baf
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-11-11 13:30:55 -0500

    refactor: add image constants file

--- view/utils/image.utils.ts
+++ view/utils/image.utils.ts
@@ -1,7 +1,7 @@
 import { API_ROOT } from '../constants/shared.constants';
 
 const VALID_IMAGE_FORMAT = /(jpe?g|png|gif|webp)$/;
-const MAX_IMAGE_SIZE = 10 * 1024 * 1024; // 10MB
+const MAX_IMAGE_SIZE = 5 * 1024 * 1024; // 5MB
 const MAX_IMAGE_COUNT = 5;
 
 export const validateImageInput = (imageInput: File | File[]) => {
