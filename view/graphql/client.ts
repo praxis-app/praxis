b5e0e18e015fca6dbc5b6f38e9d4abd20d3688c4
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-11-12 15:23:13 -0500

    refactor: restore error link for apollo client

--- view/graphql/client.ts
+++ view/graphql/client.ts
@@ -1,6 +1,8 @@
-import { ApolloClient, from } from '@apollo/client';
+import { ApolloClient, Observable, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 import { createUploadLink } from 'apollo-upload-client';
 import { Environments } from '../constants/shared.constants';
+import { formatGQLError } from '../utils/error.utils';
 import cache from './cache';
 
 const terminatingLink = createUploadLink({
@@ -8,8 +10,29 @@ const terminatingLink = createUploadLink({
   uri: '/api/graphql',
 });
 
+const errorLink = onError(
+  ({ graphQLErrors, networkError, response }) =>
+    new Observable((observer) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(async ({ message, locations, path }, index) => {
+          // TODO: Include extensions.stacktrace in error output
+          console.error(formatGQLError(message, path, locations));
+
+          if (!response) {
+            return observer.error(graphQLErrors[index]);
+          }
+          return observer.next(response);
+        });
+      }
+      if (networkError) {
+        console.error(`[Network error]: ${networkError}`);
+        return observer.error(networkError);
+      }
+    }),
+);
+
 const client = new ApolloClient({
-  link: from([terminatingLink]),
+  link: from([errorLink, terminatingLink]),
   connectToDevTools: process.env.NODE_ENV !== Environments.Production,
   cache,
 });
