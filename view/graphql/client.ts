bfe15ca6ba38d63399d95ce963a27d170760d267
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-11-12 15:40:54 -0500

    refactor: update formatGqlError to optionally include stacktrace

--- view/graphql/client.ts
+++ view/graphql/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, Observable, from } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 import { createUploadLink } from 'apollo-upload-client';
 import { Environments } from '../constants/shared.constants';
-import { formatGQLError } from '../utils/error.utils';
+import { formatGqlError } from '../utils/error.utils';
 import cache from './cache';
 
 const terminatingLink = createUploadLink({
@@ -14,15 +14,17 @@ const errorLink = onError(
   ({ graphQLErrors, networkError, response }) =>
     new Observable((observer) => {
       if (graphQLErrors) {
-        graphQLErrors.map(async ({ message, locations, path }, index) => {
-          // TODO: Include extensions.stacktrace in error output
-          console.error(formatGQLError(message, path, locations));
-
-          if (!response) {
-            return observer.error(graphQLErrors[index]);
-          }
-          return observer.next(response);
-        });
+        graphQLErrors.map(
+          async ({ message, locations, path, extensions }, index) => {
+            console.error(
+              formatGqlError(message, { extensions, locations, path }),
+            );
+            if (!response) {
+              return observer.error(graphQLErrors[index]);
+            }
+            return observer.next(response);
+          },
+        );
       }
       if (networkError) {
         console.error(`[Network error]: ${networkError}`);
