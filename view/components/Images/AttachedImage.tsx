09953d29defacbc27ee00b7a7a37e4de0a02f79b
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-11-12 20:38:30 -0500

    refactor: get image src via fetch in attached image

--- view/components/Images/AttachedImage.tsx
+++ view/components/Images/AttachedImage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { AttachedImageFragment } from '../../graphql/images/fragments/gen/AttachedImage.gen';
 import { useIsDesktop } from '../../hooks/shared.hooks';
@@ -12,19 +12,31 @@ interface Props {
 
 const AttachedImage = ({ image, marginBottom, width = '100%' }: Props) => {
   const [isLoading, setIsLoaded] = useState(true);
+  const [src, setSrc] = useState<string>();
   const isDesktop = useIsDesktop();
+
+  useEffect(() => {
+    const getImageSrc = async () => {
+      const imagePath = getImagePath(image.id);
+      const response = await fetch(imagePath, { headers: {} });
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      setSrc(url);
+    };
+    getImageSrc();
+  }, [image]);
 
   const loadingHeight = isDesktop ? '500px' : '200px';
 
   return (
     <LazyLoadImage
-      alt={image.filename}
+      src={src}
       effect="blur"
       width={width}
       height={isLoading ? loadingHeight : 'auto'}
-      onLoad={() => setIsLoaded(false)}
-      src={getImagePath(image.id)}
       style={{ display: 'block', marginBottom }}
+      onLoad={() => setIsLoaded(false)}
+      alt={image.filename}
     />
   );
 };
