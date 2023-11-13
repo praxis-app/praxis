9efac5729935f63ef2b14daeef170afafca1474f
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-11-12 22:04:42 -0500

    refactor: use useImageSrc hook in user avatar

--- view/hooks/image.hooks.ts
+++ view/hooks/image.hooks.ts
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react';
 import { getImagePath } from '../utils/image.utils';
 import { getAuthHeader } from '../graphql/client';
 
-export const useImageSrc = (imageId: number) => {
+export const useImageSrc = (imageId: number | undefined) => {
   const [src, setSrc] = useState<string>();
 
   useEffect(() => {
+    if (!imageId) {
+      return;
+    }
     const getImageSrc = async () => {
       const imagePath = getImagePath(imageId);
       const headers = { headers: { ...getAuthHeader() } };
