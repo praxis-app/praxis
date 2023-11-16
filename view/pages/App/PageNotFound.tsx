6da1f2f9fa23a17e67d1808cf4abbd120d840a29
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-11-15 21:36:39 -0500

    feat: show 404 glitch art gif on 404 page

--- view/pages/App/PageNotFound.tsx
+++ view/pages/App/PageNotFound.tsx
@@ -1,10 +1,24 @@
-import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import pageNotFoundGif from '../../assets/images/404.gif';
+import Center from '../../components/Shared/Center';
 
 const PageNotFound = () => {
   const { t } = useTranslation();
 
-  return <Typography>{t('errors.pageNotFound')}</Typography>;
+  return (
+    <Center>
+      <Box
+        alt={t('errors.pageNotFound')}
+        component="img"
+        width="50%"
+        height="50%"
+        alignSelf="center"
+        src={pageNotFoundGif}
+        sx={{ objectFit: 'cover' }}
+      />
+    </Center>
+  );
 };
 
 export default PageNotFound;
