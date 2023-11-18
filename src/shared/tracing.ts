be14c6dbd7b44075e7330feea53db9a944e95d63
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-11-17 21:38:31 -0500

    chore: setup tracing with otel

--- src/shared/tracing.ts
+++ src/shared/tracing.ts
@@ -1,16 +1,16 @@
-import {
-  ConsoleSpanExporter,
-  SimpleSpanProcessor,
-} from '@opentelemetry/sdk-trace-base';
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import * as process from 'process';
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 import { Resource } from '@opentelemetry/resources';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import * as process from 'process';
 
-const traceExporter = new ConsoleSpanExporter();
+const traceExporter = new OTLPTraceExporter({
+  url: 'http://localhost:4318/v1/traces',
+});
 
 export const otelSDK = new NodeSDK({
   resource: new Resource({
