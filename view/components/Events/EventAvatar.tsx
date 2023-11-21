36555a4f631561bf7b291fd05014b0144bda9377
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-11-20 20:39:20 -0500

    feat: show loading state for event and group avatars

--- view/components/Events/EventAvatar.tsx
+++ view/components/Events/EventAvatar.tsx
@@ -1,8 +1,10 @@
-import { Avatar, AvatarProps, useTheme } from '@mui/material';
+import { Avatar, AvatarProps, CircularProgress, useTheme } from '@mui/material';
 import { EventAvatarFragment } from '../../graphql/events/fragments/gen/EventAvatar.gen';
 import { ProposalActionEventAvatarFragment } from '../../graphql/proposals/fragments/gen/ProposalActionEventAvatar.gen';
 import { useImageSrc } from '../../hooks/image.hooks';
+import { DarkMode } from '../../styles/theme';
 import { getEventPath } from '../../utils/event.utils';
+import Center from '../Shared/Center';
 import Link from '../Shared/Link';
 
 interface Props extends AvatarProps {
@@ -23,7 +25,11 @@ const EventAvatar = ({ event, withLink, size, sx, ...avatarProps }: Props) => {
   };
 
   const renderAvatar = () => (
-    <Avatar alt={event.name} src={src} sx={avatarStyles} {...avatarProps} />
+    <Avatar alt={event.name} src={src} sx={avatarStyles} {...avatarProps}>
+      <Center bgcolor={DarkMode.PhantomShip} width="100%" height="100%">
+        <CircularProgress size={10} sx={{ alignSelf: 'center' }} />
+      </Center>
+    </Avatar>
   );
 
   if (withLink) {
--- view/components/Groups/GroupAvatar.tsx
+++ view/components/Groups/GroupAvatar.tsx
@@ -1,7 +1,9 @@
-import { Avatar, AvatarProps } from '@mui/material';
+import { Avatar, AvatarProps, CircularProgress } from '@mui/material';
 import { GroupAvatarFragment } from '../../graphql/groups/fragments/gen/GroupAvatar.gen';
 import { useImageSrc } from '../../hooks/image.hooks';
+import { DarkMode } from '../../styles/theme';
 import { getGroupPath } from '../../utils/group.utils';
+import Center from '../Shared/Center';
 import Link from '../Shared/Link';
 
 interface Props extends AvatarProps {
@@ -14,7 +16,11 @@ const GroupAvatar = ({ group }: Props) => {
 
   return (
     <Link href={groupPagePath}>
-      <Avatar src={src} alt={group.name} />
+      <Avatar src={src} alt={group.name}>
+        <Center bgcolor={DarkMode.PhantomShip} width="100%" height="100%">
+          <CircularProgress size={10} sx={{ alignSelf: 'center' }} />
+        </Center>
+      </Avatar>
     </Link>
   );
 };
