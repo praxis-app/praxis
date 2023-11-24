afc7f976bf2f0171e21d56c180301ce2c5d9fbe4
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-11-24 15:39:35 -0500

    refactor: remove forwardRef from Flex

--- view/components/Shared/Flex.tsx
+++ view/components/Shared/Flex.tsx
@@ -1,22 +1,19 @@
 import { Box, BoxProps, SxProps } from '@mui/material';
-import { ReactNode, RefObject, forwardRef } from 'react';
+import { ReactNode } from 'react';
 
 interface FlexProps extends BoxProps {
   children: string | number | ReactNode;
   flexEnd?: boolean;
 }
 
-const Flex = (
-  { children, flexEnd, sx, ...rest }: FlexProps,
-  ref: RefObject<unknown>,
-) => {
+const Flex = ({ children, flexEnd, sx, ...rest }: FlexProps) => {
   const styles: SxProps = flexEnd ? { justifyContent: 'flex-end' } : {};
 
   return (
-    <Box ref={ref} sx={{ display: 'flex', ...styles, ...sx }} {...rest}>
+    <Box sx={{ display: 'flex', ...styles, ...sx }} {...rest}>
       {children}
     </Box>
   );
 };
 
-export default forwardRef(Flex);
+export default Flex;
