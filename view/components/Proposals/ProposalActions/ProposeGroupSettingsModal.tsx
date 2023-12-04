ef4d349a3e5762b50f9c5fc15741f9cc947ab8db
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-12-03 19:00:44 -0500

    feat: add votingTimeLimit field in propose group settings modal

--- view/components/Proposals/ProposalActions/ProposeGroupSettingsModal.tsx
+++ view/components/Proposals/ProposalActions/ProposeGroupSettingsModal.tsx
@@ -10,6 +10,7 @@ import {
   DecisionMakingModel,
   ProposalActionFieldName,
   ProposalActionType,
+  VotingTimeLimit,
 } from '../../../constants/proposal.constants';
 import { ProposalActionGroupConfigInput } from '../../../graphql/gen';
 import { useGroupSettingsByGroupIdLazyQuery } from '../../../graphql/groups/queries/gen/GroupSettingsByGroupId.gen';
@@ -214,6 +215,40 @@ const ProposeGroupSettingsModal = ({
                     showPercentSign
                   />
                 </Flex>
+
+                <Divider sx={{ marginY: 3 }} />
+
+                <GroupSettingsSelect
+                  fieldName={GroupSettingsFieldName.VotingTimeLimit}
+                  label={t('groups.settings.names.votingTimeLimit')}
+                  description={t(
+                    'groups.settings.descriptions.votingTimeLimit',
+                  )}
+                  value={values.votingTimeLimit || ''}
+                  onChange={handleChange}
+                >
+                  <MenuItem value={VotingTimeLimit.HalfHour}>
+                    {t('time.minutesFull', { count: 30 })}
+                  </MenuItem>
+                  <MenuItem value={VotingTimeLimit.OneHour}>
+                    {t('time.hoursFull', { count: 1 })}
+                  </MenuItem>
+                  <MenuItem value={VotingTimeLimit.HalfDay}>
+                    {t('time.hoursFull', { count: 12 })}
+                  </MenuItem>
+                  <MenuItem value={VotingTimeLimit.OneDay}>
+                    {t('time.daysFull', { count: 1 })}
+                  </MenuItem>
+                  <MenuItem value={VotingTimeLimit.ThreeDays}>
+                    {t('time.daysFull', { count: 3 })}
+                  </MenuItem>
+                  <MenuItem value={VotingTimeLimit.OneWeek}>
+                    {t('time.weeks', { count: 1 })}
+                  </MenuItem>
+                  <MenuItem value={VotingTimeLimit.TwoWeeks}>
+                    {t('time.weeks', { count: 2 })}
+                  </MenuItem>
+                </GroupSettingsSelect>
 
                 <Divider sx={{ marginY: 3 }} />
 
