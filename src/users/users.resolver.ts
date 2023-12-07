ec460741d9551a6b0882f5b111fd167e1623a79c
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-12-06 19:45:57 -0500

    refactor: rename sync-proposals.interceptor.ts file

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -16,7 +16,7 @@ import { Group } from '../groups/models/group.model';
 import { Image } from '../images/models/image.model';
 import { Post } from '../posts/models/post.model';
 import { PostsService } from '../posts/posts.service';
-import { SyncProposalsInterceptor } from '../proposals/interceptors/syncronize-proposals.interceptor';
+import { SyncProposalsInterceptor } from '../proposals/interceptors/sync-proposals.interceptor';
 import { ServerPermissions } from '../server-roles/models/server-permissions.type';
 import { FeedItem } from '../shared/models/feed-item.union';
 import { FollowUserPayload } from './models/follow-user.payload';
