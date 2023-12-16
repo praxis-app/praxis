b8c2e458cfebeee6b416c0d2e447e7e182a52e78
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-12-15 19:55:53 -0500

    refactor: add SecurityTxtService

--- src/security-txt/security-txt.controller.ts
+++ src/security-txt/security-txt.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
-import { ServerConfigsService } from '../server-configs/server-configs.service';
+import { SecurityTxtService } from './security-txt.service';
 
 @Controller('security.txt')
 export class SecurityTxtController {
-  constructor(private serverConfigsService: ServerConfigsService) {}
+  constructor(private securityTxtService: SecurityTxtService) {}
 
   @Get('/')
   async getSecurityTxt(@Res() res: Response) {
-    const { securityTxt } = await this.serverConfigsService.getServerConfig();
-    return res.type('text/plain').send(securityTxt || 'No security.txt found');
+    const securityTxt = await this.securityTxtService.getSecurityTxt();
+    return res.type('text/plain').send(securityTxt);
   }
 }
--- src/security-txt/security-txt.module.ts
+++ src/security-txt/security-txt.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ServerConfigsModule } from '../server-configs/server-configs.module';
 import { SecurityTxtController } from './security-txt.controller';
+import { SecurityTxtService } from './security-txt.service';
 
 @Module({
   imports: [ServerConfigsModule],
   controllers: [SecurityTxtController],
+  providers: [SecurityTxtService],
 })
 export class SecurityTxtModule {}
