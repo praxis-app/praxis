8e6a1333bd98557c78c822db64d037a41f3563d6
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-12-17 14:57:03 -0500

    chore: remove migration

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -54,7 +54,6 @@ import { AddProposalActionGroupConfigTable1700964923516 } from './migrations/170
 import { AddGroupConfigColumns1701548819916 } from './migrations/1701548819916-AddGroupConfigColumns';
 import { AddSecurityTxtColumn1702684900226 } from './migrations/1702684900226-AddSecurityTxtColumn';
 import { ProposalConfig } from '../proposals/models/proposal-config.model';
-import { AddProposalConfigTable1702765481954 } from './migrations/1702765481954-AddProposalConfigTable';
 
 dotenv.config();
 
@@ -108,7 +107,6 @@ export default new DataSource({
     AddProposalActionImagesConstraint1685201083917,
     AddProposalActionRoleCascadeDelete1685748700121,
     AddProposalActionRoleTable1684893300206,
-    AddProposalConfigTable1702765481954,
     AddRoleMemberLinkTable1681172948650,
     AddSecurityTxtColumn1702684900226,
     AddServerConfigTable1699135846939,
