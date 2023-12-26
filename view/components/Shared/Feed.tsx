49537f09654ff3916d0fb091a3ab17aebe43c779
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-12-26 16:27:47 -0500

    feat: add wip pagination for Feed component

--- view/components/Shared/Feed.tsx
+++ view/components/Shared/Feed.tsx
@@ -4,10 +4,13 @@ import {
   Card,
   CardContent,
   SxProps,
+  TablePagination,
   Typography,
 } from '@mui/material';
+import { ChangeEvent, MouseEvent, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FeedItemFragment } from '../../graphql/posts/fragments/gen/FeedItem.gen';
+import { useIsDesktop } from '../../hooks/shared.hooks';
 import PostCard from '../Posts/PostCard';
 import ProposalCard from '../Proposals/ProposalCard';
 
@@ -32,7 +35,23 @@ const FeedItem = ({ item }: { item: FeedItemFragment }) => {
 };
 
 const Feed = ({ feed, ...boxProps }: Props) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
   const { t } = useTranslation();
+  const isDesktop = useIsDesktop();
+
+  const handleChangePage = (
+    _: MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => setPage(newPage);
+
+  const handleChangeRowsPerPage = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   if (feed.length === 0) {
     return (
@@ -48,6 +67,20 @@ const Feed = ({ feed, ...boxProps }: Props) => {
 
   return (
     <Box {...boxProps}>
+      <TablePagination
+        component="div"
+        count={100}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        SelectProps={{
+          sx: { display: isDesktop ? undefined : 'none' },
+        }}
+        labelRowsPerPage={isDesktop ? undefined : <></>}
+        sx={{ marginBottom: 1.5 }}
+      />
+
       {feed.map((item) => (
         <FeedItem item={item} key={`${item.__typename}-${item.id}`} />
       ))}
