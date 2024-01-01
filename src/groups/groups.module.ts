2788c34eb6e307017a0df4d156485b85ed5071a5
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-12-31 20:52:57 -0500

    refactor: remove todo comment

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -3,22 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsModule } from '../events/events.module';
 import { Image } from '../images/models/image.model';
 import { UsersModule } from '../users/users.module';
-import { GroupConfigsResolver } from './resolvers/group-configs.resolver';
-import { GroupMemberRequestsResolver } from './resolvers/group-member-requests.resolver';
 import { GroupRolesModule } from './group-roles/group-roles.module';
-import { GroupsResolver } from './resolvers/groups.resolver';
 import { GroupsService } from './groups.service';
 import { GroupConfig } from './models/group-config.model';
 import { GroupMemberRequest } from './models/group-member-request.model';
 import { Group } from './models/group.model';
+import { GroupConfigsResolver } from './resolvers/group-configs.resolver';
+import { GroupMemberRequestsResolver } from './resolvers/group-member-requests.resolver';
+import { GroupsResolver } from './resolvers/groups.resolver';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Group, GroupConfig, GroupMemberRequest, Image]),
-
-    // TODO: Remove forwardRef() when circular dependency is fixed
     forwardRef(() => UsersModule),
-
     EventsModule,
     GroupRolesModule,
   ],
