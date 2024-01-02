87867c4861a67b35bf9fb58d20696659646e36a2
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-01-02 00:35:40 -0500

    refactor: remove unneeded modules from dataloader module

--- src/dataloader/dataloader.module.ts
+++ src/dataloader/dataloader.module.ts
@@ -6,13 +6,11 @@ import { Group } from '../groups/models/group.model';
 import { Image } from '../images/models/image.model';
 import { Like } from '../likes/models/like.model';
 import { Post } from '../posts/models/post.model';
-import { PostsModule } from '../posts/posts.module';
 import { Proposal } from '../proposals/models/proposal.model';
 import { ProposalAction } from '../proposals/proposal-actions/models/proposal-action.model';
 import { ServerRole } from '../server-roles/models/server-role.model';
 import { UsersModule } from '../users/users.module';
 import { Vote } from '../votes/models/vote.model';
-import { VotesModule } from '../votes/votes.module';
 import { DataloaderService } from './dataloader.service';
 
 @Module({
@@ -29,9 +27,7 @@ import { DataloaderService } from './dataloader.service';
       ServerRole,
       Vote,
     ]),
-    PostsModule,
     UsersModule,
-    VotesModule,
   ],
   providers: [DataloaderService],
   exports: [DataloaderService],
