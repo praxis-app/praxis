f1c9c5ab30cbca148c8f28a94bd684bdf56c1056
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-01-04 21:46:14 -0500

    feat: add required columns for notifications

--- src/notifications/models/notification.model.ts
+++ src/notifications/models/notification.model.ts
@@ -1,10 +1,14 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 import {
+  Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { User } from '../../users/models/user.model';
+import { NotificationStatus } from '../notifications.constants';
 
 @ObjectType()
 @Entity()
@@ -12,6 +16,22 @@ export class Notification {
   @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  @Field()
+  message: string;
+
+  @Column()
+  @Field()
+  actionType: string;
+
+  @Column({ default: NotificationStatus.Unread })
+  @Field()
+  status: string;
+
+  @Field(() => User)
+  @ManyToOne(() => User, (user) => user.notifications, { onDelete: 'CASCADE' })
+  user: User;
 
   @CreateDateColumn()
   @Field()
---
+++ src/notifications/notifications.constants.ts
@@ -0,0 +1,11 @@
+export enum NotificationStatus {
+  Read = 'read',
+  Unread = 'unread',
+}
+
+export enum NotificationActionType {
+  PostLike = 'post-like',
+  PostComment = 'post-comment',
+  ProposalComment = 'proposal-comment',
+  ProposalVote = 'proposal-vote',
+}
