aada832db62285b2fa00c057ba323beff428cf67
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-01-14 12:22:22 -0500

    refactor: pass box props to LikeBadge

--- view/components/Likes/LikeBadge.tsx
+++ view/components/Likes/LikeBadge.tsx
@@ -1,27 +1,26 @@
 import { ThumbUp } from '@mui/icons-material';
-import { Box } from '@mui/material';
+import { Box, BoxProps } from '@mui/material';
 import { Blurple } from '../../styles/theme';
 
-const LikeBadge = () => {
-  return (
-    <Box
-      bgcolor={Blurple.Marina}
-      borderRadius="50%"
-      display="inline-flex"
-      justifyContent="center"
-      width="22px"
-      height="22px"
-    >
-      <ThumbUp
-        sx={{
-          fontSize: 13,
-          marginTop: 0.55,
-          marginLeft: '1px',
-          color: 'text.primary',
-        }}
-      />
-    </Box>
-  );
-};
+const LikeBadge = (boxProps: BoxProps) => (
+  <Box
+    bgcolor={Blurple.Marina}
+    borderRadius="50%"
+    display="inline-flex"
+    justifyContent="center"
+    width="22px"
+    height="22px"
+    {...boxProps}
+  >
+    <ThumbUp
+      sx={{
+        fontSize: 13,
+        marginTop: 0.55,
+        marginLeft: '1px',
+        color: 'text.primary',
+      }}
+    />
+  </Box>
+);
 
 export default LikeBadge;
