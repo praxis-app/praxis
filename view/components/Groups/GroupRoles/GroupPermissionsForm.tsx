ec9b9f89c60e6eec7c6c40b3c86b352f0aa60056
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-01-20 12:02:26 -0500

    refactor: use isLoading prop for PrimaryActionButton

--- view/components/Groups/GroupRoles/GroupPermissionsForm.tsx
+++ view/components/Groups/GroupRoles/GroupPermissionsForm.tsx
@@ -1,20 +1,11 @@
-/**
- * TODO: Add remaining functionality - below is a WIP
- *
- * Permissions that still need to be implemented fully:
- * - Manage Events
- * - Manage Comments
- * - Remove Members
- */
-
-import { Box, BoxProps, CircularProgress } from '@mui/material';
+import { Box, BoxProps } from '@mui/material';
 import { Form, Formik, FormikHelpers } from 'formik';
 import { useTranslation } from 'react-i18next';
+import { GROUP_PERMISSION_NAMES } from '../../../constants/role.constants';
 import { toastVar } from '../../../graphql/cache';
 import { GroupRolePermissionInput } from '../../../graphql/gen';
 import { GroupRolePermissionsFragment } from '../../../graphql/groups/fragments/gen/GroupRolePermissions.gen';
 import { useUpdateGroupRoleMutation } from '../../../graphql/groups/mutations/gen/UpdateGroupRole.gen';
-import { GROUP_PERMISSION_NAMES } from '../../../constants/role.constants';
 import Flex from '../../Shared/Flex';
 import PrimaryActionButton from '../../Shared/PrimaryActionButton';
 import GroupPermissionToggle from './GroupPermissionToggle';
@@ -72,11 +63,7 @@ const GroupPermissionsForm = ({ permissions, roleId, ...boxProps }: Props) => {
             <Flex justifyContent="end" sx={{ marginTop: 6 }}>
               <PrimaryActionButton
                 disabled={isSubmitting || loading || !dirty}
-                startIcon={
-                  loading && (
-                    <CircularProgress size={10} sx={{ marginRight: 0.25 }} />
-                  )
-                }
+                isLoading={isSubmitting || loading}
                 type="submit"
               >
                 {t('actions.save')}
--- view/components/Roles/ServerRoles/ServerPermissionsForm.tsx
+++ view/components/Roles/ServerRoles/ServerPermissionsForm.tsx
@@ -1,11 +1,11 @@
-import { Box, BoxProps, CircularProgress } from '@mui/material';
+import { Box, BoxProps } from '@mui/material';
 import { Form, Formik, FormikHelpers } from 'formik';
 import { useTranslation } from 'react-i18next';
+import { SERVER_PERMISSION_NAMES } from '../../../constants/role.constants';
 import { toastVar } from '../../../graphql/cache';
 import { ServerRolePermissionInput } from '../../../graphql/gen';
 import { ServerRolePermissionsFragment } from '../../../graphql/roles/fragments/gen/ServerRolePermissions.gen';
 import { useUpdateServerRoleMutation } from '../../../graphql/roles/mutations/gen/UpdateServerRole.gen';
-import { SERVER_PERMISSION_NAMES } from '../../../constants/role.constants';
 import Flex from '../../Shared/Flex';
 import PrimaryActionButton from '../../Shared/PrimaryActionButton';
 import ServerPermissionToggle from './ServerPermissionToggle';
@@ -63,11 +63,7 @@ const ServerPermissionsForm = ({ permissions, roleId, ...boxProps }: Props) => {
             <Flex justifyContent="end" sx={{ marginTop: 6 }}>
               <PrimaryActionButton
                 disabled={isSubmitting || loading || !dirty}
-                startIcon={
-                  loading && (
-                    <CircularProgress size={10} sx={{ marginRight: 0.25 }} />
-                  )
-                }
+                isLoading={isSubmitting || loading}
                 type="submit"
               >
                 {t('actions.save')}
