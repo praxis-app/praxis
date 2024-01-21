c3ac264abcccca4ba93025273796800777ee2b3d
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-01-20 21:52:55 -0500

    feat: add create question button

--- view/pages/Questions/VibeCheck.tsx
+++ view/pages/Questions/VibeCheck.tsx
@@ -1,12 +1,17 @@
 import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import Flex from '../../components/Shared/Flex';
+import GhostButton from '../../components/Shared/GhostButton';
 import LevelOneHeading from '../../components/Shared/LevelOneHeading';
 import ProgressBar from '../../components/Shared/ProgressBar';
 import { useServerQuestionsQuery } from '../../graphql/questions/queries/gen/ServerQuestions.gen';
+import { useIsDesktop } from '../../hooks/shared.hooks';
 
 const VibeCheck = () => {
   const { data, loading, error } = useServerQuestionsQuery();
+
   const { t } = useTranslation();
+  const isDesktop = useIsDesktop();
 
   if (error) {
     return <Typography>{t('errors.somethingWentWrong')}</Typography>;
@@ -18,9 +23,17 @@ const VibeCheck = () => {
 
   return (
     <>
-      <LevelOneHeading header>
-        {t('questions.labels.vibeCheck')}
-      </LevelOneHeading>
+      <Flex justifyContent="space-between" alignItems="center">
+        <LevelOneHeading header>
+          {t('questions.labels.vibeCheck')}
+        </LevelOneHeading>
+
+        <GhostButton sx={{ marginBottom: 3.5 }}>
+          {isDesktop
+            ? t('questions.labels.createQuestion')
+            : t('actions.create')}
+        </GhostButton>
+      </Flex>
 
       {JSON.stringify(data)}
     </>
