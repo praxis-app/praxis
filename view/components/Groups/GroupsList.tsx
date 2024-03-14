ae772d3ee478c1b5009aa3d6c74b6c75b7193cad
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-03-13 23:33:10 -0400

    feat: add tabs on groups page

--- view/components/Groups/GroupsList.tsx
+++ view/components/Groups/GroupsList.tsx
@@ -1,25 +1,63 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Tab, Tabs, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import GroupCard from '../../components/Groups/GroupCard';
-import { DEFAULT_PAGE_SIZE } from '../../constants/shared.constants';
+import {
+  DEFAULT_PAGE_SIZE,
+  NavigationPaths,
+  TAB_QUERY_PARAM,
+} from '../../constants/shared.constants';
 import { useGroupsLazyQuery } from '../../graphql/groups/queries/gen/Groups.gen';
+import { useIsDesktop } from '../../hooks/shared.hooks';
 import { isDeniedAccess } from '../../utils/error.utils';
 import LevelOneHeading from '../Shared/LevelOneHeading';
 import Pagination from '../Shared/Pagination';
 import GroupForm from './GroupForm';
 
+enum GroupsPageTabs {
+  Activity = 'activity',
+  AllGroups = 'all-groups',
+  Joined = 'joined',
+}
+
 const GroupsList = () => {
   const [rowsPerPage, setRowsPerPage] = useState(DEFAULT_PAGE_SIZE);
   const [page, setPage] = useState(0);
+  const [tab, setTab] = useState(0);
 
   const [getGroups, { data, loading, error }] = useGroupsLazyQuery({
     errorPolicy: 'all',
   });
 
+  const [searchParams] = useSearchParams();
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const isDesktop = useIsDesktop();
+
+  const tabParam = searchParams.get('tab');
+
+  const pathPrefix = `${NavigationPaths.Groups}${TAB_QUERY_PARAM}`;
+  const allGroupsTab = `${pathPrefix}${GroupsPageTabs.AllGroups}`;
+  const joinedTab = `${pathPrefix}${GroupsPageTabs.Joined}`;
+
+  const tabsStyles = {
+    '& .MuiTabs-centered': {
+      gap: isDesktop ? '20px' : '10px',
+    },
+  };
 
   useEffect(() => {
+    if (!tabParam) {
+      setTab(0);
+    }
+    if (tabParam === GroupsPageTabs.AllGroups) {
+      setTab(1);
+    }
+    if (tabParam === GroupsPageTabs.Joined) {
+      setTab(2);
+    }
+
     getGroups({
       variables: {
         input: {
@@ -28,7 +66,7 @@ const GroupsList = () => {
         },
       },
     });
-  }, [rowsPerPage, page, getGroups]);
+  }, [rowsPerPage, page, getGroups, tabParam]);
 
   const onChangePage = async (newPage: number) => {
     await getGroups({
@@ -53,6 +91,23 @@ const GroupsList = () => {
       <LevelOneHeading header>
         {t('groups.headers.exploreGroups')}
       </LevelOneHeading>
+
+      <Card>
+        <Tabs sx={tabsStyles} textColor="inherit" value={tab} centered>
+          <Tab
+            label={t('groups.tabs.activity')}
+            onClick={() => navigate(NavigationPaths.Groups)}
+          />
+          <Tab
+            label={t('groups.tabs.allGroups')}
+            onClick={() => navigate(allGroupsTab)}
+          />
+          <Tab
+            label={t('groups.tabs.joined')}
+            onClick={() => navigate(joinedTab)}
+          />
+        </Tabs>
+      </Card>
 
       <GroupForm />
 
