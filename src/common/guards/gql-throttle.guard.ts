39d5768b1a4989a0d04b992261b8723802081f9c
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-03-23 23:57:44 -0400

    fix: add getTracker for throttle guard with logging

--- src/common/guards/gql-throttle.guard.ts
+++ src/common/guards/gql-throttle.guard.ts
@@ -1,15 +1,28 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, Logger } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Injectable()
 export class GqlThrottlerGuard extends ThrottlerGuard {
+  private readonly logger = new Logger(GqlThrottlerGuard.name);
+
   getRequestResponse(context: ExecutionContext) {
     const gqlCtx = GqlExecutionContext.create(context);
     const ctx = gqlCtx.getContext();
     return { req: ctx.req, res: ctx.res };
   }
-  async getErrorMessage() {
+
+  protected async getTracker(req: Record<string, any>) {
+    this.logger.log(
+      `Getting tracker IP: ${req.ips.length ? req.ips[0] : req.ip}`,
+    );
+    this.logger.log(
+      `X-Forwarded-For header: ${req?.headers['X-Forwarded-For']}`,
+    );
+    return req.ips.length ? req.ips[0] : req.ip;
+  }
+
+  protected async getErrorMessage() {
     return 'Too many requests';
   }
 }
