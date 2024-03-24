bb317373708ba47cc0f68e6aff1e682e0ccbdf68
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-03-24 14:22:50 -0400

    test: log user email in getErrorMessage

--- src/common/guards/gql-throttler.guard.ts
+++ src/common/guards/gql-throttler.guard.ts
@@ -1,20 +1,39 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { ThrottlerGuard } from '@nestjs/throttler';
+import { Request } from 'express';
+import { LoginInput } from '../../auth/models/login.input';
+
+interface LoginRequest extends Request {
+  body: {
+    variables: { input: LoginInput };
+  };
+}
+
+interface LoginContext extends ExecutionContext {
+  req: LoginRequest;
+  res: Response;
+}
 
 @Injectable()
 export class GqlThrottlerGuard extends ThrottlerGuard {
   getRequestResponse(context: ExecutionContext) {
-    const gqlCtx = GqlExecutionContext.create(context);
-    const ctx = gqlCtx.getContext();
+    const ctx = this.getContext(context);
     return { req: ctx.req, res: ctx.res };
   }
 
-  protected async getTracker(req: Record<string, any>) {
+  async getTracker(req: LoginRequest) {
     return req.ips.length ? req.ips[0] : req.ip;
   }
 
-  protected async getErrorMessage() {
+  async getErrorMessage(context: ExecutionContext) {
+    const ctx = this.getContext(context);
+    console.log(ctx.req.body.variables.input.email);
     return 'Too many requests';
+  }
+
+  private getContext(context: ExecutionContext) {
+    const gqlCtx = GqlExecutionContext.create(context);
+    return gqlCtx.getContext<LoginContext>();
   }
 }
