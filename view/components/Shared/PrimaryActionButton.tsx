0b77d3181edcbc25b94fd90e9bc34ff96ab99b2b
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-04-16 22:25:46 -0400

    refactor: remove BLURPLE_BUTTON_COLORS const

--- view/components/Shared/PrimaryActionButton.tsx
+++ view/components/Shared/PrimaryActionButton.tsx
@@ -1,43 +1,44 @@
-import { Button as MuiButton, ButtonProps, styled } from '@mui/material';
+import { Button, ButtonProps, SxProps, useTheme } from '@mui/material';
 import { Blurple, DarkMode } from '../../styles/theme';
 import Spinner from './Spinner';
-
-const BLURPLE_BUTTON_COLORS = {
-  backgroundColor: Blurple.Marina,
-  '&:active': {
-    backgroundColor: Blurple.Marina,
-  },
-  '&:hover': {
-    backgroundColor: Blurple.SavoryBlue,
-  },
-  '&:disabled': {
-    backgroundColor: DarkMode.Liver,
-  },
-};
-
-const Button = styled(MuiButton)(({ theme }) => ({
-  ...BLURPLE_BUTTON_COLORS,
-  color: theme.palette.common.white,
-  fontFamily: 'Inter Bold',
-  letterSpacing: '0.2px',
-  textTransform: 'none',
-  borderRadius: 9999,
-  padding: '0 15px',
-  minWidth: 85,
-  height: 38,
-}));
 
 interface Props extends ButtonProps {
   isLoading?: boolean;
 }
 
-const PrimaryActionButton = ({ isLoading, children, ...props }: Props) => (
-  <Button {...props}>
-    {isLoading && (
-      <Spinner size={10} color="inherit" sx={{ marginRight: 0.75 }} />
-    )}
-    {children}
-  </Button>
-);
+const PrimaryActionButton = ({ isLoading, children, sx, ...props }: Props) => {
+  const theme = useTheme();
+
+  const buttonStyles: SxProps = {
+    backgroundColor: Blurple.Marina,
+    '&:active': {
+      backgroundColor: Blurple.Marina,
+    },
+    '&:hover': {
+      backgroundColor: Blurple.SavoryBlue,
+    },
+    '&:disabled': {
+      backgroundColor: DarkMode.Liver,
+    },
+    color: theme.palette.common.white,
+    fontFamily: 'Inter Bold',
+    letterSpacing: '0.2px',
+    textTransform: 'none',
+    borderRadius: 9999,
+    padding: '0 15px',
+    minWidth: 85,
+    height: 38,
+    ...sx,
+  };
+
+  return (
+    <Button sx={buttonStyles} {...props}>
+      {isLoading && (
+        <Spinner size={10} color="inherit" sx={{ marginRight: 0.75 }} />
+      )}
+      {children}
+    </Button>
+  );
+};
 
 export default PrimaryActionButton;
