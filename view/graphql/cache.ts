f3e9b6769f9e90499dd127998122f3492290831d
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-04-20 14:42:39 -0400

    feat: set correct query name in cache

--- view/graphql/cache.ts
+++ view/graphql/cache.ts
@@ -67,7 +67,7 @@ const cache = new InMemoryCache({
             return incoming;
           },
         },
-        serverQuestionnaireTickets: {
+        questionnaireTickets: {
           merge(_, incoming) {
             return incoming;
           },
