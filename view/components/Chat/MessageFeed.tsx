ff897ba6f86a8986c1dc9d46b5a538bfebd827c3
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-04-27 00:49:20 -0400

    feat: show messages in scrollable container that stops short of form

--- view/components/Chat/MessageFeed.tsx
+++ view/components/Chat/MessageFeed.tsx
@@ -1,4 +1,6 @@
+import { Box, Container, SxProps, useTheme } from '@mui/material';
 import { MessageFragment } from '../../graphql/chat/fragments/gen/Message.gen';
+import { useIsDesktop } from '../../hooks/shared.hooks';
 import Message from './Message';
 
 interface Props {
@@ -6,12 +8,34 @@ interface Props {
 }
 
 const MessageFeed = ({ messages }: Props) => {
+  const isDesktop = useIsDesktop();
+  const theme = useTheme();
+
+  const containerStyles: SxProps = {
+    [theme.breakpoints.up('xs')]: {
+      paddingX: 0,
+      paddingTop: '30px',
+      paddingBottom: isDesktop ? '6px' : '12px',
+    },
+    [theme.breakpoints.up('md')]: {
+      paddingTop: '55px',
+    },
+    position: 'fixed',
+    top: isDesktop ? '60px' : '55px',
+    bottom: isDesktop ? '215px' : '100px',
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    overflowY: 'scroll',
+  };
+
   return (
-    <>
-      {messages.map((message) => (
-        <Message key={message.id} message={message} />
-      ))}
-    </>
+    <Container maxWidth="sm" sx={containerStyles}>
+      <Box>
+        {messages.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </Box>
+    </Container>
   );
 };
 
