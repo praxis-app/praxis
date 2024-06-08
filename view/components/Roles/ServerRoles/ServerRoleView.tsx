f23a632a66cbcf4799f857214d0f913e08b4e60a
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-06-08 19:58:44 -0400

    feat: show role members in server role view

--- view/components/Roles/ServerRoles/ServerRoleView.tsx
+++ view/components/Roles/ServerRoles/ServerRoleView.tsx
@@ -9,10 +9,15 @@ import {
   styled,
 } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { truncate } from 'lodash';
 import { ServerRoleViewFragment } from '../../../graphql/roles/fragments/gen/ServerRoleView.gen';
 import { PermissionName, getPermissionText } from '../../../utils/role.utils';
 import Flex from '../../Shared/Flex';
 import { useIsDesktop } from '../../../hooks/shared.hooks';
+import { TruncationSizes } from '../../../constants/shared.constants';
+import UserAvatar from '../../Users/UserAvatar';
+import { getUserProfilePath } from '../../../utils/user.utils';
+import Link from '../../Shared/Link';
 
 const CardHeader = styled(MuiCardHeader)(() => ({
   paddingBottom: 0,
@@ -127,6 +132,52 @@ const ServerRoleView = ({ role }: Props) => {
                   >
                     <Close sx={{ color: '#e04f4a' }} /> {displayName}
                   </Flex>
+                );
+              })}
+            </Flex>
+          </>
+        )}
+
+        {role.members.length > 0 && (
+          <>
+            <Typography
+              fontFamily="Inter Bold"
+              marginBottom={1.2}
+              textTransform="uppercase"
+              fontSize={15}
+            >
+              {t('roles.labels.roleMembers')}
+            </Typography>
+
+            <Flex
+              flexDirection={isDesktop ? 'row' : 'column'}
+              flexWrap={isDesktop ? 'wrap' : 'nowrap'}
+              gap={isDesktop ? 1.6 : 1.4}
+              marginBottom={1.25}
+              paddingLeft={isDesktop ? 0.1 : 0.5}
+            >
+              {role.members.map((member) => {
+                const username = member.displayName || member.name;
+                const truncatedUsername = truncate(username, {
+                  length: isDesktop
+                    ? TruncationSizes.Small
+                    : TruncationSizes.Medium,
+                });
+                const userPath = getUserProfilePath(member.name);
+
+                return (
+                  <Link
+                    key={member.id}
+                    href={userPath}
+                    sx={{
+                      width: isDesktop ? '235px' : '100%',
+                      display: 'flex',
+                      gap: 1.3,
+                    }}
+                  >
+                    <UserAvatar user={member} size={25} />
+                    {truncatedUsername}
+                  </Link>
                 );
               })}
             </Flex>
