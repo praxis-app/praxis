406cb51113f07c2094bddd15e8f8eeb5647387e2
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-06-08 17:20:56 -0400

    feat: add link to view roles page on roles page

--- view/pages/Roles/ServerRolesIndex.tsx
+++ view/pages/Roles/ServerRolesIndex.tsx
@@ -1,10 +1,14 @@
 import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { useServerRolesQuery } from '../../graphql/roles/queries/gen/ServerRoles.gen';
+import { useNavigate } from 'react-router-dom';
 import RoleList from '../../components/Roles/RoleList';
 import ServerRoleForm from '../../components/Roles/ServerRoles/ServerRoleForm';
+import Flex from '../../components/Shared/Flex';
+import GhostButton from '../../components/Shared/GhostButton';
 import LevelOneHeading from '../../components/Shared/LevelOneHeading';
 import ProgressBar from '../../components/Shared/ProgressBar';
+import { NavigationPaths } from '../../constants/shared.constants';
+import { useServerRolesQuery } from '../../graphql/roles/queries/gen/ServerRoles.gen';
 import { isDeniedAccess } from '../../utils/error.utils';
 
 const ServerRolesIndex = () => {
@@ -15,6 +19,7 @@ const ServerRolesIndex = () => {
   const canManageRoles = permissions?.manageRoles;
 
   const { t } = useTranslation();
+  const navigate = useNavigate();
 
   if (isDeniedAccess(error) || (permissions && !canManageRoles)) {
     return <Typography>{t('prompts.permissionDenied')}</Typography>;
@@ -30,7 +35,18 @@ const ServerRolesIndex = () => {
 
   return (
     <>
-      <LevelOneHeading header>{t('roles.headers.serverRoles')}</LevelOneHeading>
+      <Flex justifyContent="space-between" alignItems="center">
+        <LevelOneHeading header>
+          {t('roles.headers.serverRoles')}
+        </LevelOneHeading>
+
+        <GhostButton
+          onClick={() => navigate(NavigationPaths.ViewRoles)}
+          sx={{ marginBottom: 3.5 }}
+        >
+          {t('roles.labels.quickView')}
+        </GhostButton>
+      </Flex>
 
       <ServerRoleForm />
       {roles && <RoleList roles={roles} />}
