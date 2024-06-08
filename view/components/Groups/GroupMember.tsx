9a11675297d56f761c410ad2332be64c07cc9ed2
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-06-08 14:49:43 -0400

    fix: gracefully handle long usernames in group members page

--- view/components/Groups/GroupMember.tsx
+++ view/components/Groups/GroupMember.tsx
@@ -1,5 +1,8 @@
 import { styled, Typography } from '@mui/material';
+import { truncate } from 'lodash';
+import { TruncationSizes } from '../../constants/shared.constants';
 import { GroupMemberFragment } from '../../graphql/groups/fragments/gen/GroupMember.gen';
+import { useIsDesktop } from '../../hooks/shared.hooks';
 import { getUserProfilePath } from '../../utils/user.utils';
 import SharedFlex from '../Shared/Flex';
 import Link from '../Shared/Link';
@@ -18,21 +21,28 @@ interface Props {
   currentUserId: number;
 }
 
-const GroupMember = ({ member, currentUserId }: Props) => (
-  <Flex sx={{ justifyContent: 'space-between' }}>
-    <Link href={getUserProfilePath(member.name)}>
-      <Flex>
-        <UserAvatar user={member} sx={{ marginRight: 1.5 }} />
-        <Typography sx={{ marginTop: 1 }}>
-          {member.displayName || member.name}
-        </Typography>
-      </Flex>
-    </Link>
+const GroupMember = ({ member, currentUserId }: Props) => {
+  const isDesktop = useIsDesktop();
 
-    {currentUserId !== member.id && (
-      <FollowButton user={member} currentUserId={currentUserId} />
-    )}
-  </Flex>
-);
+  const username = member.displayName || member.name;
+  const truncatedUsername = truncate(username, {
+    length: isDesktop ? TruncationSizes.Medium : 22,
+  });
+
+  return (
+    <Flex sx={{ justifyContent: 'space-between' }}>
+      <Link href={getUserProfilePath(member.name)}>
+        <Flex>
+          <UserAvatar user={member} sx={{ marginRight: 1.5 }} />
+          <Typography sx={{ marginTop: 1 }}>{truncatedUsername}</Typography>
+        </Flex>
+      </Link>
+
+      {currentUserId !== member.id && (
+        <FollowButton user={member} currentUserId={currentUserId} />
+      )}
+    </Flex>
+  );
+};
 
 export default GroupMember;
