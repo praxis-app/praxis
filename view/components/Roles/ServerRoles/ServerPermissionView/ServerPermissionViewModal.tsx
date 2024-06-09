631547c10b28c14b4e2b699b935b6b9418c2e7ba
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-06-09 13:10:09 -0400

    feat: show roles with permission in ServerPermissionViewModal

--- view/components/Roles/ServerRoles/ServerPermissionView/ServerPermissionViewModal.tsx
+++ view/components/Roles/ServerRoles/ServerPermissionView/ServerPermissionViewModal.tsx
@@ -2,6 +2,8 @@ import { Box, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useServerPermissionViewModalLazyQuery } from '../../../../graphql/roles/queries/gen/ServerPermissionViewModal.gen';
+import { useIsDesktop } from '../../../../hooks/shared.hooks';
+import Flex from '../../../Shared/Flex';
 import Modal from '../../../Shared/Modal';
 import ProgressBar from '../../../Shared/ProgressBar';
 import ServerRoleMembersView from '../ServerRoleMembersView';
@@ -25,6 +27,7 @@ const ServerPermissionViewModal = ({
     useServerPermissionViewModalLazyQuery();
 
   const { t } = useTranslation();
+  const isDesktop = useIsDesktop();
 
   useEffect(() => {
     if (isOpen && !called) {
@@ -54,10 +57,45 @@ const ServerPermissionViewModal = ({
       {loading && <ProgressBar />}
 
       {data && (
-        <ServerRoleMembersView
-          members={data.membersWithPermission}
-          header={t('permissions.headers.membersWithPermission')}
-        />
+        <>
+          <Typography
+            fontFamily="Inter Bold"
+            marginBottom={0.75}
+            textTransform="uppercase"
+            fontSize={15}
+          >
+            {t('permissions.headers.rolesWithPermission')}
+          </Typography>
+          <Flex
+            flexDirection={isDesktop ? 'row' : 'column'}
+            flexWrap={isDesktop ? 'wrap' : 'nowrap'}
+            gap={0.9}
+            marginBottom={2.25}
+            paddingLeft={0.4}
+          >
+            {data.serverRoles.map((role) => (
+              <Flex
+                key={role.id}
+                width={isDesktop ? '240px' : '100%'}
+                alignItems="center"
+                gap={1.3}
+              >
+                <Box
+                  bgcolor={role.color}
+                  width="20px"
+                  height="20px"
+                  borderRadius={9999}
+                />
+                {role.name}
+              </Flex>
+            ))}
+          </Flex>
+
+          <ServerRoleMembersView
+            members={data.membersWithPermission}
+            header={t('permissions.headers.membersWithPermission')}
+          />
+        </>
       )}
     </Modal>
   );
