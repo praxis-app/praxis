7968d6bd6722c160245b232da7827a77a38ef5cd
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-06-30 21:17:59 -0400

    feat: add model relations for shared proposals

--- view/components/Proposals/ProposalCardFooter.tsx
+++ view/components/Proposals/ProposalCardFooter.tsx
@@ -12,7 +12,6 @@ import { useSyncProposalMutation } from '../../graphql/proposals/mutations/gen/S
 import { useProposalCommentsLazyQuery } from '../../graphql/proposals/queries/gen/ProposalComments.gen';
 import { useIsProposalRatifiedSubscription } from '../../graphql/proposals/subscriptions/gen/IsProposalRatified.gen';
 import { useInView } from '../../hooks/shared.hooks';
-import { inDevToast } from '../../utils/shared.utils';
 import CommentForm from '../Comments/CommentForm';
 import CommentsList from '../Comments/CommentList';
 import CardFooterButton from '../Shared/CardFooterButton';
@@ -148,9 +147,7 @@ const ProposalCardFooter = ({
     height: '24px',
   };
 
-  const handleVoteButtonClick = (
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
+  const handleVoteBtnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     if (!isLoggedIn) {
       toastVar({
         status: 'info',
@@ -189,7 +186,7 @@ const ProposalCardFooter = ({
     setMenuAnchorEl(event.currentTarget);
   };
 
-  const handleCommentButtonClick = async () => {
+  const handleCommentBtnClick = async () => {
     if (inModal || isProposalPage) {
       return;
     }
@@ -204,6 +201,24 @@ const ProposalCardFooter = ({
     }
   };
 
+  const handleShareBtnClick = () => {
+    // if (hasMissingSharedPost) {
+    //   toastVar({
+    //     title: t('posts.errors.missingContentShare'),
+    //     status: 'info',
+    //   });
+    //   return;
+    // }
+    if (!isVerified) {
+      toastVar({
+        title: t('posts.prompts.verifyToShare'),
+        status: 'info',
+      });
+      return;
+    }
+    // setIsShareModalOpen(true);
+  };
+
   return (
     <Box ref={ref}>
       <Flex
@@ -216,7 +231,7 @@ const ProposalCardFooter = ({
         {!!commentCount && (
           <Typography
             color="text.secondary"
-            onClick={handleCommentButtonClick}
+            onClick={handleCommentBtnClick}
             sx={commentCountStyles}
           >
             {t('comments.labels.xComments', { count: commentCount })}
@@ -233,18 +248,18 @@ const ProposalCardFooter = ({
           menuAnchorEl={menuAnchorEl}
           myVoteId={myVote?.id}
           myVoteType={myVote?.voteType}
-          onClick={handleVoteButtonClick}
+          onClick={handleVoteBtnClick}
           proposalId={proposal.id}
           setMenuAnchorEl={setMenuAnchorEl}
           decisionMakingModel={settings.decisionMakingModel}
         />
 
-        <CardFooterButton onClick={handleCommentButtonClick}>
+        <CardFooterButton onClick={handleCommentBtnClick}>
           <Comment sx={ROTATED_ICON_STYLES} />
           {t('actions.comment')}
         </CardFooterButton>
 
-        <CardFooterButton onClick={inDevToast}>
+        <CardFooterButton onClick={handleShareBtnClick}>
           <Reply sx={ROTATED_ICON_STYLES} />
           {t('actions.share')}
         </CardFooterButton>
