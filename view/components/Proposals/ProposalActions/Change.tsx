5ca6e879f79ecc81f252227388836b85fcff5657
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-07-05 00:57:55 -0400

    feat: account for long text in Change

--- view/components/Proposals/ProposalActions/Change.tsx
+++ view/components/Proposals/ProposalActions/Change.tsx
@@ -1,8 +1,9 @@
 import { SxProps, Typography, useTheme } from '@mui/material';
+import { ReactNode } from 'react';
 import { ChangeType } from '../../../constants/shared.constants';
+import { useIsDesktop } from '../../../hooks/shared.hooks';
 import Flex from '../../Shared/Flex';
 import ChangeIcon from './ChangeIcon';
-import { ReactNode } from 'react';
 
 interface Props {
   value?: string | number | null;
@@ -12,6 +13,7 @@ interface Props {
 
 const Change = ({ value, valueIcon, changeType }: Props) => {
   const theme = useTheme();
+  const isDesktop = useIsDesktop();
 
   const changeStyles: SxProps = {
     borderColor: theme.palette.divider,
@@ -28,7 +30,16 @@ const Change = ({ value, valueIcon, changeType }: Props) => {
     <Flex sx={changeStyles}>
       <ChangeIcon changeType={changeType} sx={{ marginRight: '0.8ch' }} />
       {valueIcon}
-      <Typography color="primary" fontSize="inherit" marginRight="0.25ch">
+      <Typography
+        color="primary"
+        fontSize="inherit"
+        marginRight="0.25ch"
+        overflow="hidden"
+        textOverflow="ellipsis"
+        whiteSpace="nowrap"
+        width={isDesktop ? '85%' : '240px'}
+        title={value?.toString()}
+      >
         {value}
       </Typography>
     </Flex>
