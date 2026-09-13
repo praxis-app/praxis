e2c34dbca772fbc4b0533e07d776cd0a9f01ab7e
Forrest Wilkins <ethanforrestwilkins@gmail.com>
2026-09-13 01:05:05 -0400

    refactor: remove comment about default CONTENT_ROOT

--- api/src/common/storage.rs
+++ api/src/common/storage.rs
@@ -1,7 +1,6 @@
 use std::path::{Path, PathBuf};
 
 pub(crate) fn upload_root() -> PathBuf {
-    // Default to the project's content directory when CONTENT_ROOT is unset
     std::env::var("CONTENT_ROOT")
         .map(PathBuf::from)
         .unwrap_or_else(|_| {
